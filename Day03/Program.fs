b0751bd57b8df2c99b44fe122fa1e13eac7c8c49
Linus Nilsson <121645+lmonilsson@users.noreply.github.com>
2024-12-03 19:24:18 +0100

    Day 03

---
+++ Day03/Program.fs
@@ -0,0 +1,33 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let part1 input =
+    let matches = Regex.Matches(input, @"mul\(([0-9]{1,3}),([0-9]{1,3})\)")
+    let result =
+        matches
+        |> Seq.map (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value))
+        |> Seq.sum
+
+    printfn $"Part 1: {result}"
+
+
+let part2 input =
+    let matches = Regex.Matches(input, @"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)")
+
+    let mutable active = true
+    let mutable result = 0
+    for m in matches do
+        match m.Value with
+        | "do()" -> active <- true
+        | "don't()" -> active <- false
+        | _ ->
+            if active then
+                result <- result + ((int m.Groups[1].Value) * (int m.Groups[2].Value))
+
+    printfn $"Part 2: {result}"
+
+
+// Entry point
+let input = File.ReadAllText("input.txt")
+part1 input
+part2 input
